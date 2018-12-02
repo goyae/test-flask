5b4cf5194a8f41b6c22a20d66c3da4d0f0fe28f4
goyae <44313778+goyae@users.noreply.github.com>
2018-12-02 14:26:18 +0900

    Use variable

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Hello world."""
-    html = render_template('index.html')
+    html = render_template('index.html', text='text message')
     return html
 
 if __name__ == "__main__":
